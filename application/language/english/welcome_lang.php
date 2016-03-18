838eabf421f0754fe2dfa6163e692c22e623c953
bw <karasawa@cross-law.jp>
2016-03-18 12:07:59 +0000

    remove tag

--- application/language/english/welcome_lang.php
+++ application/language/english/welcome_lang.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 
 $lang['warning_message'] = function(){
     return 'So long as this message remains in the code, the project is NOT production ready, and should only be used with the Bitcoin Testnet. Let ye be warned.';
